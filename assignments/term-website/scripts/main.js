d1aac1f76884b0f1d521d68f97ff31c1a5a998ac
Hernan! <her.rodri44@gmail.com>
2018-04-05 23:43:38 -0300

    Service page

---
+++ assignments/term-website/scripts/main.js
@@ -0,0 +1,11 @@
+
+var obj = new XMLHttpRequest();
+
+var reqUrl = 'data/service-data.json';
+obj.open('GET', reqUrl, true);
+obj.send();
+
+obj.onload = function(){
+    var objInfo = JSON.parse(obj.responseText);
+    console.log(objInfo);
+}
