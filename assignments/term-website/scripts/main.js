25a5584670b63da02c0f912d58c3da50a46620b5
Hernan! <her.rodri44@gmail.com>
2018-04-05 23:58:54 -0300

    json test 3

--- assignments/term-website/scripts/main.js
+++ assignments/term-website/scripts/main.js
@@ -1,14 +1,21 @@
-
 var obj = new XMLHttpRequest();
 
 var reqUrl = 'data/service-data.json';
 obj.open('GET', reqUrl, true);
 obj.send();
 
-obj.onload = function(){
+obj.onload = function () {
     var objInfo = JSON.parse(obj.responseText);
-    console.log("testing obj 1");
-    console.log(objInfo.services[1]);
- 
-    
+    console.log("testing obj jquery .each()");
+
+    $.each(objInfo.services, function (index, element) {
+        $('.tbody').append(
+                '<tr><td>'+ element.description +'</td><td>'+ element.price +'</td></tr>'
+        );
+    });
+
+    $('.tbody > tr > td').each(function (index, element) {
+        $(element).css('background-color', 'rgb(100,200,0)');
+    });
+
 }
