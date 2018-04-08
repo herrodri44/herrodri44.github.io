b8a1881f8d3881f9b5bd3b0d583267c6cafe0bfb
Hernan! <her.rodri44@gmail.com>
2018-04-07 22:26:39 -0300

    external gmap js

--- assignments/term-website/scripts/gMap.js
+++ assignments/term-website/scripts/gMap.js
@@ -1,3 +1,10 @@
 
 
 
+var myMap = function() {
+            var mapProp = {
+                center: new google.maps.LatLng(45.6722766, -111.0929062),
+                zoom: 15,
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        }
